86e064b696b48f2be1ffb756b2f00b092428d60d
Volodymyr Rykhva <volodymyr.rykhva@sigma.software>
2018-11-09 17:04:13 +0200

    modified structure of a proj

--- CardGuru/CardGuru/Controller/Login/TouchLoginViewController.swift
+++ CardGuru/CardGuru/Controller/Login/TouchLoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TouchLoginViewController: UIViewController {
+final class TouchLoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
